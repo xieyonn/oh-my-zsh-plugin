c11958a4a5788e20520bf3d834945eca5e7ee7f1
xieyonn <qxieyongp@163.com>
2022-04-30 19:25:00 +0800

    feat add git config

--- xieyonn.plugin.zsh
+++ xieyonn.plugin.zsh
@@ -7,3 +7,13 @@ function shell_proxy_promt_info() {
 }
 
 alias gtidy='git reset --hard && git clean -df'
+alias tailf='tail -f'
+
+#LESS COLOR
+export LESS_TERMCAP_mb=$(printf '\e[01;31m') # enter blinking mode – red
+export LESS_TERMCAP_md=$(printf '\e[01;35m') # enter double-bright mode – bold, magenta
+export LESS_TERMCAP_me=$(printf '\e[0m') # turn off all appearance modes (mb, md, so, us)
+export LESS_TERMCAP_se=$(printf '\e[0m') # leave standout mode
+export LESS_TERMCAP_so=$(printf '\e[01;33m') # enter standout mode – yellow
+export LESS_TERMCAP_ue=$(printf '\e[0m') # leave underline mode
+export LESS_TERMCAP_us=$(printf '\e[04;36m') # enter underline mode – cyan
